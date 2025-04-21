4a5245b0e38b2828d6b4511026a512dcc8217e8a
Rishav <rishishah907@gmail.com>
2025-04-22 02:01:44 +0530

    add ssl option for db

--- splitfree_backend/settings.py
+++ splitfree_backend/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', ''),
+        'OPTIONS': os.getenv('DB_OPTIONS', {}),
     }
 }
 
