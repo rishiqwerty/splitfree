57fb133f6af7dda6de482e1e76e6ac680d2f57dd
Rishav <rishishah907@gmail.com>
2025-04-22 11:12:27 +0530

    render fix

--- splitfree_backend/settings.py
+++ splitfree_backend/settings.py
@@ -83,12 +83,6 @@ WSGI_APPLICATION = 'splitfree_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-try:
-    db_options = json.loads(os.getenv('DB_OPTIONS', '{}'))  # Parse DB_OPTIONS as JSON
-except json.JSONDecodeError as e:
-    print(f"Error parsing DB_OPTIONS: {e}")  # Log the error for debugging
-    db_options = {} 
     
 DATABASES = {
     'default': {
