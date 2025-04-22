56500eea96caebbdb5efe8b02fb842c6b70e23c0
Rishav <rishishah907@gmail.com>
2025-04-22 10:48:17 +0530

    render fix

--- splitfree_backend/settings.py
+++ splitfree_backend/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', ''),
-        'OPTIONS': db_options,
+        'OPTIONS': {"sslmode": os.getenv('DB_OPTIONS', '')},
     }
 }
 
